940d083c2d7c504e8815ccfc5817b889a7167864
Ana Paula Percino Huerta <128005733+pauph@users.noreply.github.com>
2023-10-04 20:19:24 -0600

    Create expar2ej3.c

---
+++ expar2ej3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, lineas;
+    
+    printf("Numero de lineas en la piramide: ");
+    scanf("%d", &lineas);
+    for(i=1;i<=lineas;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
